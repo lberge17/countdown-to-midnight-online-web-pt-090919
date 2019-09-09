dba1c97e75b4efdeda6742bdaa1194f86765a61b
Laura Berge <lauraeberge@gmail.com>
2019-09-09 15:18:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,6 @@ def countdown(n)
   until n == 0 do
     puts "#{n} SECOND(S)!"
     n -= 1
-    countdown_with_sleep(n)
   end
   if n == 0
     return "HAPPY NEW YEAR!"
@@ -12,5 +11,12 @@ def countdown(n)
 end
 
 def countdown_with_sleep(n)
-  sleep n
+    until n == 0 do
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1
+  end
+  if n == 0
+    return "HAPPY NEW YEAR!"
+  end
 end
