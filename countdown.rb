87f8072e9a124fc38bfb1cdbae641e7a7cc4731b
Laura Berge <lauraeberge@gmail.com>
2019-09-09 15:11:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,13 +4,10 @@ def countdown(n)
   until n == 0 do
     if n == 1
       puts "#{n} SECOND"
+      puts "HAPPY NEW YEAR"
     else
       puts "#{n} SECONDS"
     end
     n -= 1
-  end
-  if n == 0
-    puts "HAPPY NEW YEAR!"
-    return "HAPPY NEW YEAR!"
   end
 end
